dbacb069ce2d4fd5ef0526e6b16462362e91fe2f
Subransu <maharanasubhransu6@gmail.com>
2025-06-15 11:29:43 +0530

    Adding intial code

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className='flex h-screen flex-col items-center justify-center'>
+      <h1 className='text-3xl text-brand'>Storiva - the one and only solution</h1>
+    </div>
+  );
+}
