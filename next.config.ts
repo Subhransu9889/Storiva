6d0b40dd02d6f40a6b789237a600a93e7b8fb2e8
Subransu <maharanasubhransu6@gmail.com>
2025-06-20 21:13:06 +0530

    Implement the card component for upload files

--- next.config.ts
+++ next.config.ts
@@ -26,6 +26,11 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "cloud.appwrite.io",
       },
+
+      {
+        protocol: "https",
+        hostname: 'fra.cloud.appwrite.io',
+      },
     ],
   },
 };
