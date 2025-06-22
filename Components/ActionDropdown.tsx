84f3efea12ab8ed3a1cb064f9369e21e7824abd6
Subransu <maharanasubhransu6@gmail.com>
2025-06-22 11:07:42 +0530

    Implemented rename functionality

--- Components/ActionDropdown.tsx
+++ Components/ActionDropdown.tsx
@@ -22,6 +22,8 @@ import Link from "next/link";
 import {constructDownloadUrl} from "@/lib/utils";
 import {Input} from "@/Components/ui/input";
 import {Button} from "@/Components/ui/button";
+import {usePathname} from "next/navigation";
+import {renameFile} from "@/lib/actions/file.action";
 
 const ActionDropdown = ({file}: {file: Models.Document}) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -37,8 +39,26 @@ const ActionDropdown = ({file}: {file: Models.Document}) => {
     setName(file.name);
     setIsLoading(false);
   };
+  const path = usePathname();
 
-  const handleAction = async () => {}
+  const handleAction = async () => {
+    if(!action) return;
+    setIsLoading(true);
+    let success = false;
+
+    const actions = {
+      rename: () => renameFile({fileId: file.$id, name, extension: file.extension, path}),
+      share: () => console.log('share'),
+      delete: () => console.log('delete'),
+      details: () => console.log('details'),
+    };
+    success = await actions[action.value as keyof typeof actions]();
+
+    if(success) {
+      closeAllModals();
+    }
+    setIsLoading(false);
+  }
 
   const renderDialogContent = () => {
     if(!action) return null;
--- lib/actions/file.action.ts
+++ lib/actions/file.action.ts
@@ -78,4 +78,24 @@ export const getFiles = async () => {
     } catch (error) {
         handleError(error, "Failed to get files")
     }
+};
+
+
+export const renameFile = async ({fileId, name, extension, path}: RenameFileProps) => {
+    const {databases} = await createAdminClient();
+    try{
+        const newName = `${name}.${extension}`;
+        const updateFile = await databases.updateDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.filesCollectionId,
+            fileId,
+            {
+                name: newName,
+            }
+        );
+        revalidatePath(path);
+        return parseStringify(updateFile);
+    } catch (error) {
+        handleError(error, "Failed to rename file")
+    }
 }
